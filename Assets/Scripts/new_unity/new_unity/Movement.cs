3416d8736874ad051a55e7f3573f19218969cca5
dakuritsu <Pierre.eknb@gmail.com>
2024-02-13 10:22:42 +0100

    update
    rip si j'ai remplacé des mauvais trucs

--- Assets/Scripts/new_unity/new_unity/Movement.cs
+++ Assets/Scripts/new_unity/new_unity/Movement.cs
@@ -2,30 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//LateUpdate appelé avant que les collisions ne soient calculées donc on choisit FixedUpdate()
-
 public class Movement : MonoBehaviour
 {
     public float speed;
     public Animator animator;
-    private Vector3 direction; // car sinon on peut pas l'utiliser dans FixedUpdate()
+    private Endurance endurance;    
+
+
+    private void Start()
+    {
+        endurance = GetComponent<Endurance>();
+    }
 
     private void Update(){
         float horizontal = Input.GetAxisRaw("Horizontal"); // Q D -> <-
         float vertical = Input.GetAxisRaw("Vertical"); // Z S flèche du haut flèche du bas
 
-        direction = new Vector3(horizontal, vertical, 0);
+        Vector3 direction = new Vector3(horizontal, vertical);
 
         AnimateMovement(direction);
 
-
+        transform.position += direction * GetSpeed() * Time.deltaTime;
     }
 
-    private void FixedUpdate(){
-        transform.position += direction * speed * Time.deltaTime;
+    private float GetSpeed()
+    {
+        return Input.GetKey(KeyCode.LeftShift) && endurance.currentEndurance > 0 ? speed * 2f : speed;
     }
 
-    private void AnimateMovement(Vector3 direction){
+    void AnimateMovement(Vector3 direction){
         if(animator != null)
         {
             if(direction.magnitude > 0)
