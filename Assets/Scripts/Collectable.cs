257cdca05aaa70c5768a99bf3014ef0fbad4b0d7
dakuritsu <Pierre.eknb@gmail.com>
2024-02-26 21:04:11 +0100

    ajout de collectable et de l'inventaire
    (pas fini)                                                              ajout de collectable et de l'inventaire         pouvoir stocker les collectables dans un inventaire

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class Collectable : MonoBehaviour
 {
+    public CollectableType type;
+    public Sprite icon;
     private void OnTriggerEnter2D(Collider2D collision){
         Player player = collision.GetComponent<Player>();
 
         if(player)
         {
-            player.numGold++;
+            player.inventory.Add(this);
             Destroy(this.gameObject);
         }
     }
+}
+
+public enum CollectableType // enum avec les différent types 
+{
+    NONE, 
+    GOLD
 }
---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Inventory
+{
+    [System.Serializable]
+    public class Slot   
+    {
+        public CollectableType type;
+        public int count;
+        public int maxAllowed;
+        public Sprite icon;
+        public Slot()   // Constructeur Slot 
+        {
+            type = CollectableType.NONE;
+            count = 0;
+            maxAllowed = 60;
+        }
+
+        public bool CanAddItem()    // vérifie si il y a encore de la place
+        {
+            if(count < maxAllowed)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void AddItem(Collectable item) //modifie le type et l'icon du slot
+        {
+            this.type = item.type;
+            this.icon = item.icon;
+            count++;
+        }
+        
+    }
+
+    public List<Slot> slots = new List<Slot>();
+
+    public Inventory(int numSlots)  // Constructeur Inventory initialise chaque case 
+    {
+        for(int i=0 ; i < numSlots ; i++)
+        {
+            Slot slot = new Slot();
+            slots.Add(slot);
+        }
+    } 
+
+    public void Add(Collectable item)// ajoute le collectable si on peut
+    {
+        foreach(Slot slot in slots)
+        {
+            if(slot.type  == item.type && slot.CanAddItem())
+            {
+                slot.AddItem(item);
+                return;
+            }
+        }
+
+        foreach(Slot slot in slots)
+        {
+            if(slot.type == CollectableType.NONE)
+            {
+                slot.AddItem(item);
+                return;
+            }
+        }
+    }
+}
