508b5121124b94068fe19f9ca7d428820da2b805
dakuritsu <74517832+Dakuritsu@users.noreply.github.com>
2024-02-27 17:53:37 +0100

    Ajout du PlayerController pour la class Player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,12 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(PlayerController))]
+
 public class Player : MonoBehaviour
 {   
+    private PlayerController playerController;
     public Inventory inventory;
 
     private void Awake()
     {
         inventory = new Inventory(21);
-    } 
+    }
+
+    private void Start()
+    {
+        playerController = GetComponent<PlayerController>();
+    }
 }
