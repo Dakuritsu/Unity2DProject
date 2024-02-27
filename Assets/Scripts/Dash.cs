b306fd037c625f4d296bc4da91701239e9469439
dakuritsu <74517832+Dakuritsu@users.noreply.github.com>
2024-02-27 17:55:39 +0100

    Nouveau dash

--- Assets/Scripts/Dash.cs
+++ Assets/Scripts/Dash.cs
@@ -1,54 +1,77 @@
 using UnityEngine;
+using UnityEngine.Events;   
 using System.Collections;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Dash : MonoBehaviour
 {
     private bool canDash = true;
     private bool isDashing;
     private Vector2 dashingDir;
     private Endurance endurance;
-
+    private Rigidbody2D rb;
+ 
+    [SerializeField] private int dashingCost = 30;
     [SerializeField] private float dashingPower = 28f;
-    [SerializeField] private float dashingTime = 0.2f;
-    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float dashingTime = 0.5f;
+    [SerializeField] private float dashTimer = 0f;
     [SerializeField] private TrailRenderer tr;
+
+    [SerializeField] private UnityEvent DashCompleted;
 
 
     private void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         endurance = GetComponent<Endurance>();
     }
-    
-    private void FixedUpdate()
+
+
+
+    private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E) && canDash && endurance.GetCurrentEndurance >= 30f)
+        if (isDashing)
         {
+            dashTimer += Time.deltaTime;
+            if (dashTimer >= dashingTime)
+            {
+                StopDashing();
+            }
+            else
+            {
+                dashingDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+                if (dashingDir == Vector2.zero)
+                {
+                    dashingDir = new Vector2(transform.localScale.x, 0f);
+                }
+
+                rb.velocity = dashingDir.normalized * dashingPower;
+            }
+        }
+    }
+
+    public bool TryDash()
+    {
+        if (canDash && endurance.GetCurrentEndurance >= dashingCost)
+        {
+            endurance.ConsumeEnduranceForDash(dashingCost);
             isDashing = true;
             canDash = false;
             tr.emitting = true;
-            dashingDir = new Vector2(Input.GetAxisRaw("Horizontal") , Input.GetAxisRaw("Vertical"));
+            dashTimer = 0f;
 
-            if(dashingDir == Vector2.zero)
-            {
-                dashingDir = new Vector2(transform.localScale.x, 0f);
-            }
-            StartCoroutine(StopDashing());
-            endurance.ConsumeEnduranceForDash(30f);
+            return true;
         }
-        
-        if(isDashing)
-        {
-            rb.velocity = dashingDir.normalized * dashingPower;
-            return;
-        }
+
+        return false;
     }
 
-    private IEnumerator StopDashing()
+    private void StopDashing()
     {
-        yield return new WaitForSeconds(dashingTime);
         rb.velocity = Vector2.zero;
         tr.emitting = false;
         isDashing = false;
         canDash = true;
+        DashCompleted.Invoke();
     }
 }
