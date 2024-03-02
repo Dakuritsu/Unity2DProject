8bf6e989ae81950b256a02967fa63f6ca42041bf
dakuritsu <Pierre.eknb@gmail.com>
2024-03-02 19:59:37 +0100

    Ajout du player qui peux changer de scene et aller dans la Base

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -8,9 +8,42 @@ public class Portal : MonoBehaviour
     public int SceneIndex;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.CompareTag("Player"))
         {
-            SceneManager.LoadScene(SceneIndex, LoadSceneMode.Single);
+            if (SceneIndex == 2)// Si on veut charger la scene "Base"
+            {
+                LoadInteriorSceneAndTransferPlayer();
+            }
+            else// Sinon on charge la scene normalement
+            {
+                SceneManager.LoadScene(SceneIndex, LoadSceneMode.Single);
+            }
         }
     }
+
+    private void LoadInteriorSceneAndTransferPlayer()
+    {
+        // Charger la scène intérieure
+        SceneManager.LoadScene(SceneIndex, LoadSceneMode.Single);
+
+        // Récupérer une référence au joueur
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+        if (player != null)
+        {
+            // Déplacer le joueur dans la nouvelle scène
+            SceneManager.MoveGameObjectToScene(player, SceneManager.GetSceneByBuildIndex(SceneIndex));
+
+            // Éviter que le joueur ne soit détruit lorsque la scène est changée
+            DontDestroyOnLoad(player);
+
+            // Définir la nouvelle position du joueur
+            //player.transform.position = new Vector3(-2.5f, -2.5f);
+        }
+        else
+        {
+            Debug.LogWarning("Player not found");
+        }
+    }
+
 }
