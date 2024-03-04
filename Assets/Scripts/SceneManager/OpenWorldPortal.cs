0cff342106916626f48e2e8947cbd5dc73903606
AmineChafii <chafiimedamine@gmail.com>
2024-03-04 03:32:17 +0100

    Adjustement du script du portail pour corriger la fonction de positionnement du joueur et ajout du portail de la maison

---
+++ Assets/Scripts/SceneManager/OpenWorldPortal.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpenWorldPortal : MonoBehaviour
+{
+    public int SceneIndex;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (SceneIndex == 2)// Si on veut charger la scene "Base"
+            {
+                LoadInteriorSceneAndTransferPlayer();
+            }
+            else// Sinon on charge la scene normalement
+            {
+                SceneManager.LoadScene(SceneIndex, LoadSceneMode.Single);
+            }
+        }
+    }
+
+    private void LoadInteriorSceneAndTransferPlayer()
+    {
+        // Charger la scène consernée 
+        SceneManager.LoadScene(SceneIndex, LoadSceneMode.Single);
+        // Récupérer une référence au joueur
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+        if (player != null)
+        {
+            // Si le joueur existe dans la nouvelle scène, on précise juste sa position
+            player.transform.position = new Vector3(-2.5f, -3f);
+        }
+        else
+        {
+            // Sinon on récupére une référence au joueur dans la scène d'origine
+            GameObject playerInPreviousScene = GameObject.FindGameObjectWithTag("Player");
+
+            if (playerInPreviousScene != null)
+            {
+                // Instancier le joueur dans la nouvelle scène
+                player = Instantiate(playerInPreviousScene);
+
+                //On déplace le joueur dans la nouvelle scène 
+                SceneManager.MoveGameObjectToScene(player, SceneManager.GetSceneByBuildIndex(SceneIndex));
+
+                // On Définit la nouvelle position du joueur 
+                player.transform.position = new Vector3(-2.5f, -3f);
+
+                // On évite que le joueur ne soit détruit lorsque la svène est changée 
+                DontDestroyOnLoad(player);
+            }
+            else
+            {
+                Debug.LogWarning("Player not found in the previous scene.");
+            }
+        }
+    }
+
+}
