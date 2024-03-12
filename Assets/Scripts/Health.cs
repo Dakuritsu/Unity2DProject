d0cb671ca23fdc8fd85a2b4e115689904838e690
Rayane Bensaid <rayane_bnsd@outlook.fr>
2024-03-12 16:16:21 +0100

    Annulation des deux derniers commits

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,11 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
     public float maxHealth = 100f;
     private float currentHealth;
+
+    // Définir un événement pour signaler les changements de santé du joueur
+    public delegate void HealthChanged(float currentHealth, float maxHealth);
+    public event HealthChanged OnHealthChanged;
 
     private void Start()
     {
@@ -19,6 +21,10 @@ public class Health : MonoBehaviour
 
         Debug.Log("Dommages du joueur " + damageAmount + " Current health: " + currentHealth);
 
+        // Déclencher l'événement de changement de santé
+        if (OnHealthChanged != null)
+            OnHealthChanged(currentHealth, maxHealth);
+
         if (currentHealth <= 0f)
         {
             Die();
@@ -30,4 +36,15 @@ public class Health : MonoBehaviour
         Debug.Log("Player died.");
         //gameObject.SetActive(false);
     }
+
+    public float GetCurrentHealth()
+    {
+        return this.currentHealth;
+    }
+
+    public float GetMaxHealth()
+    {
+        return this.maxHealth;
+    }
 }
+
