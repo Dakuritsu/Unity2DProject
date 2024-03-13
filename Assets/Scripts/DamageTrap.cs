957bbdba5ed19fcdc800b4956e46edfeacc8066c
Rayane Bensaid <rayane_bnsd@outlook.fr>
2024-03-13 13:09:49 +0100

    vie du joueur + pieges

--- Assets/Scripts/DamageTrap.cs
+++ Assets/Scripts/DamageTrap.cs
@@ -2,24 +2,24 @@ using UnityEngine;
 
 public class DamageTrap : MonoBehaviour
 {
-    // Dommage infligé à chaque collision avec la zone de piège
-    public float dommage = 10f;
+    public float damage = 10f;
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        // Vérifie si l'objet en collision est le joueur
-        if (other.CompareTag("Player"))
-        {
-            // Récupère le script de santé du joueur
-            Health playerHealth = other.GetComponent<Health>();
+ 
+        Player player = collision.GetComponent<Player>();
 
-            // Vérifie si le script de santé existe sur le joueur
+        if (player != null)
+        {
+            
+            Health playerHealth = player.GetComponent<Health>();
+
+          
             if (playerHealth != null)
             {
-                // Applique les dommages au joueur en utilisant la fonction ReduceDamage du script de santé
-                playerHealth.ReduceDamage(dommage);
+                
+                playerHealth.ReduceDamage(damage);
             }
         }
     }
 }
-
